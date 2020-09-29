7645aa8b783caf6da1cac744ac291cfee19cbb51
Jitendra Patel <41681183+ammipatel@users.noreply.github.com>
2020-09-29 17:09:36 +0530

    [Jitendra] Added: Validate Last Name

--- Login.java
+++ Login.java
@@ -4,20 +4,20 @@ import java.util.regex.Matcher;
 
 public class Login{
 	public static void main(String[] args){
-	System.out.println("Enter your First Name");
+	System.out.println("Enter your Last Name");
 	Scanner sc=new Scanner(System.in);
-	String first_name=sc.nextLine();
+	String last_name=sc.nextLine();
 	
 	Pattern pattern = Pattern.compile("^[A-Z]{1}(?=.*[a-z]).{2,}$"); //pin code is 6 digit string
-	Matcher m=pattern.matcher(first_name);                                       
+	Matcher m=pattern.matcher(last_name);                                       
 	int i=0;
 
 	while(m.find()){
-		System.out.println("Valid First Name");
+		System.out.println("Valid Last Name");
 		i=1;
 	}
 	 if(i==0){
-		System.out.println("Invalid First Name");	
+		System.out.println("Invalid Last Name");	
 	}
 
 	
