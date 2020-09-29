10654aed8d64eb6ce68f711e1a9f98d1004ada3d
Jitendra Patel <41681183+ammipatel@users.noreply.github.com>
2020-09-29 17:17:19 +0530

    [Jitendra] Added : Validate Email

--- Login.java
+++ Login.java
@@ -4,20 +4,20 @@ import java.util.regex.Matcher;
 
 public class Login{
 	public static void main(String[] args){
-	System.out.println("Enter your Last Name");
+	System.out.println("Enter your Email");
 	Scanner sc=new Scanner(System.in);
-	String last_name=sc.nextLine();
+	String email=sc.nextLine();
 	
-	Pattern pattern = Pattern.compile("^[A-Z]{1}(?=.*[a-z]).{2,}$"); //pin code is 6 digit string
-	Matcher m=pattern.matcher(last_name);                                       
+	Pattern pattern = Pattern.compile("^[_A-Za-z0-9+-]+(\\.[_A-Za-z0-9-]+)*@" + "[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$"); 
+	Matcher m=pattern.matcher(email);                                       
 	int i=0;
 
 	while(m.find()){
-		System.out.println("Valid Last Name");
+		System.out.println("Valid Email");
 		i=1;
 	}
 	 if(i==0){
-		System.out.println("Invalid Last Name");	
+		System.out.println("Invalid Email");	
 	}
 
 	
